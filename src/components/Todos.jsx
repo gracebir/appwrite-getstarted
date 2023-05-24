e02745c996971a1bd55347cb0aef0c7b184e890c
gracebir <grace.devolop@gmail.com>
2023-05-24 19:43:56 +0200

    listing all todos

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { database } from '../appwrite/appwrite'
+
 
 function Todos() {
+  const [loader, setLoader] = useState(true)
+  const [todos, setTodos] = useState([])
+  useEffect(()=> {
+    const list = database.listDocuments("646c965f9aab5dbc918f","646c9725b8b4dbe37488")
+    list.then(
+      (response)=> {
+        setTodos(response.documents)
+        setLoader(false)
+      },
+      (error)=>{
+        console.log(error)
+      }
+    )
+  }, [])
   return (
-    <div>
-      
-    </div>
+    <div className="max-w-7xl mx-auto">
+    <p className="text-xl font-bold mb-2">Todo List</p>
+    {loader ? (
+      <p>Loading ...</p>
+    ) : (
+      <div>
+        {todos.map((todo, i)=> (
+          <div key={i}>
+          <div className="p-4 flex items-center justify-between border-b-2 bg-gray-100 rounded-lg mb-1">
+            <div>
+              <p>{todo.text}</p>
+            </div>
+            <div>
+              <span
+                className="text-red-400 cursor-pointer"
+                
+              >
+                Delete
+              </span>
+            </div>
+          </div>
+        </div>
+        ) )}
+            
+      </div>
+    )}
+  </div>
   )
 }
 
