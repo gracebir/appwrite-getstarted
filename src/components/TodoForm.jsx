d61175712e42d1f253ab7cfba9cf3ef5a4f66450
gracebir <grace.devolop@gmail.com>
2023-05-24 19:42:54 +0200

    working on todo form and handle submittion

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {  database } from '../appwrite/appwrite'
+import { v4 as uuid } from 'uuid';
 
 function TodoForm() {
+  const [text, setText] = useState("");
+
+  // handlesubmit
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const promise = database.createDocument("646c965f9aab5dbc918f","646c9725b8b4dbe37488", uuid(), {
+      text
+    })
+    promise.then(
+      (response)=>{
+        console.log(response)
+        setText("")
+      },
+      (error)=>{console.log(error)}
+      )
+     e.target.reset() 
+  }
   return (
-    <div>
-      
+    <div className="max-w-7xl mx-auto mt-10">
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        className="flex justify-center mb-10"
+      >
+        <input
+          type="text"
+          name="todo"
+          placeholder="Enter Todo"
+          className="border p-2 w-2/3 rounded-md"
+          onChange={(e)=> setText(e.target.value)}
+        />
+        <button
+          className="bg-purple-500 p-2 text-white ml-2 rounded-md"
+          type="submit"
+        >
+          Add Todo
+        </button>
+      </form>
     </div>
   )
 }
