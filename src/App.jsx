715fc200bb6c822cd03d748e23d5c3e74c951161
gracebir <grace.devolop@gmail.com>
2023-05-24 08:35:01 +0200

    adding profile and todo routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Routes, Route } from 'react-router'
 import Signup from './components/Signup'
 import Login from './components/Login'
+import Profile from './components/Profile'
+import Todos from './components/Todos'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Signup/>}/>
       <Route path='/login' element={<Login/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/todos' element={<Todos/>}/>
     </Routes>
   )
 }
